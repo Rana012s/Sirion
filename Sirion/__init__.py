f7737743ea40c47c96ea8dbc265f0fbb8043ef56
ExoticHero <126850195+ExoticHero@users.noreply.github.com>
2023-08-06 03:26:28 +0530

    Sirion

--- Sirion/__init__.py
+++ Sirion/__init__.py
@@ -1,10 +1,9 @@
 import time
+import config
 from aiohttp import ClientSession
 from config import HELPABLE, BOT_ID, BOT_NAME, BOT_USERNAME, BOT_MENTION
 from Sirion.core.git import git
 from Sirion.core.dir import dirr
-from Sirion.core.bot import MusicBot
-from Sirion.core.userbot import Userbot
 from Sirion.misc import dbb, heroku, sudo
 from Sirion.platforms import YouTubeAPI, SpotifyAPI, TeleAPI
 
@@ -22,9 +21,9 @@ heroku()
 sudo()
 
 
-app = MusicBot()
+app = config.app
 
-userbot = Userbot()
+userbot = config.userbot
 
 YouTube = YouTubeAPI()
 
