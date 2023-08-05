2487a9ce3eae4685e7dc7d542e76e198c36fabc1
ExoticHero <126850195+ExoticHero@users.noreply.github.com>
2023-08-05 12:27:40 +0530

    Sirion

--- Sirion/core/userbot.py
+++ Sirion/core/userbot.py
@@ -1,9 +1,9 @@
 import sys
 import config
-
 from pyrogram import Client
-
 from Sirion.logging import LOGGER
+from Sirion import BOT_NAME
+
 
 assistants = []
 assistantids = []
@@ -23,31 +23,13 @@ class Userbot(Client):
             session_name=str(config.STRING2),
             no_updates=True,
         )
-        self.three = Client(
-            api_id=config.API_ID,
-            api_hash=config.API_HASH,
-            session_name=str(config.STRING3),
-            no_updates=True,
-        )
-        self.four = Client(
-            api_id=config.API_ID,
-            api_hash=config.API_HASH,
-            session_name=str(config.STRING4),
-            no_updates=True,
-        )
-        self.five = Client(
-            api_id=config.API_ID,
-            api_hash=config.API_HASH,
-            session_name=str(config.STRING5),
-            no_updates=True,
-        )
-
+        
     async def start(self):
-        LOGGER(__name__).info(f"Getting Assistants Info...")
+        LOGGER(__name__).info(f"Getting Assistants Info")
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("TheAltron")
+                await self.one.join_chat("TeamSirion")
             except:
                 pass
             assistants.append(1)
@@ -63,18 +45,18 @@ class Userbot(Client):
             LOGGER(__name__).info(f"Assistant Started as {self.one.name}")
             try:
                 await self.one.send_message(
-                    config.LOG_GROUP_ID, f"**» {config.MUSIC_BOT_NAME} ᴀssɪsᴛᴀɴᴛ ᴏɴᴇ sᴛᴀʀᴛᴇᴅ :**\n\n❄ ɴᴀᴍᴇ : {self.one.name}\n✨ ɪᴅ : `{self.one.id}`\n💫 ᴜsᴇʀɴᴀᴍᴇ : @{self.one.username}"
+                    config.LOG_GROUP_ID, f"**» {BOT_NAME} ᴀssɪsᴛᴀɴᴛ ᴏɴᴇ sᴛᴀʀᴛᴇᴅ :**\n\n❄ ɴᴀᴍᴇ : {self.one.name}\n✨ ɪᴅ : `{self.one.id}`\n💫 ᴜsᴇʀɴᴀᴍᴇ : @{self.one.username}"
                 )
             except:
                 LOGGER(__name__).error(
-                    f"Assistant Account 1 has failed to access the log Group. Make sure that you have added your assistant to your log group and promoted as admin ! "
+                    f"Assistant Account 1 has failed to access the log Group"
                 )
                 sys.exit()
 
         if config.STRING2:
             await self.two.start()
             try:
-                await self.one.join_chat("TheAltron")
+                await self.one.join_chat("TeamSirion")
             except:
                 pass
             assistants.append(2)
@@ -89,92 +71,11 @@ class Userbot(Client):
                 self.two.name = get_me.first_name
             try:
                 await self.two.send_message(
-                    config.LOG_GROUP_ID, f"**» {config.MUSIC_BOT_NAME} ᴀssɪsᴛᴀɴᴛ ᴛᴡᴏ sᴛᴀʀᴛᴇᴅ :**\n\n❄ ɴᴀᴍᴇ : {self.two.name}\n✨ ɪᴅ : `{self.two.id}`\n💫 ᴜsᴇʀɴᴀᴍᴇ : @{self.two.username}"
+                    config.LOG_GROUP_ID, f"**» {BOT_NAME} ᴀssɪsᴛᴀɴᴛ ᴛᴡᴏ sᴛᴀʀᴛᴇᴅ :**\n\n❄ ɴᴀᴍᴇ : {self.two.name}\n✨ ɪᴅ : `{self.two.id}`\n💫 ᴜsᴇʀɴᴀᴍᴇ : @{self.two.username}"
                 )
             except:
                 LOGGER(__name__).error(
-                    f"Assistant Account 2 has failed to access the log Group. Make sure that you have added your assistant to your log group and promoted as admin ! "
+                    f"Assistant Account 2 has failed to access the log Group"
                 )
                 sys.exit()
             LOGGER(__name__).info(f"Assistant Two Started as {self.two.name}")
-
-        if config.STRING3:
-            await self.three.start()
-            try:
-                await self.one.join_chat("TheAltron")
-            except:
-                pass
-            assistants.append(3)
-            get_me = await self.three.get_me()
-            self.three.username = get_me.username
-            self.three.id = get_me.id
-            self.three.mention = get_me.mention
-            assistantids.append(get_me.id)
-            if get_me.last_name:
-                self.three.name = get_me.first_name + " " + get_me.last_name
-            else:
-                self.three.name = get_me.first_name
-            try:
-                await self.three.send_message(
-                    config.LOG_GROUP_ID, f"**» {config.MUSIC_BOT_NAME} ᴀssɪsᴛᴀɴᴛ ᴛʜʀᴇᴇ sᴛᴀʀᴛᴇᴅ :**\n\n❄ ɴᴀᴍᴇ : {self.three.name}\n✨ ɪᴅ : `{self.three.id}`\n💫 ᴜsᴇʀɴᴀᴍᴇ : @{self.three.username}"
-                )
-            except:
-                LOGGER(__name__).error(
-                    f"Assistant Account 3 has failed to access the log Group. Make sure that you have added your assistant to your log group and promoted as admin ! "
-                )
-                sys.exit()
-            LOGGER(__name__).info(f"Assistant Three Started as {self.three.name}")
-
-        if config.STRING4:
-            await self.four.start()
-            try:
-                await self.one.join_chat("TheAltron")
-            except:
-                pass
-            assistants.append(4)
-            get_me = await self.four.get_me()
-            self.four.username = get_me.username
-            self.four.id = get_me.id
-            self.four.mention = get_me.mention
-            assistantids.append(get_me.id)
-            if get_me.last_name:
-                self.four.name = get_me.first_name + " " + get_me.last_name
-            else:
-                self.four.name = get_me.first_name
-            try:
-                await self.four.send_message(
-                    config.LOG_GROUP_ID, f"**» {config.MUSIC_BOT_NAME} ᴀssɪsᴛᴀɴᴛ ғᴏᴜʀ sᴛᴀʀᴛᴇᴅ :**\n\n❄ ɴᴀᴍᴇ : {self.four.name}\n✨ ɪᴅ : `{self.four.id}`\n💫 ᴜsᴇʀɴᴀᴍᴇ : @{self.four.username}"
-                )
-            except:
-                LOGGER(__name__).error(
-                    f"Assistant Account 4 has failed to access the log Group. Make sure that you have added your assistant to your log group and promoted as admin ! "
-                )
-                sys.exit()
-            LOGGER(__name__).info(f"Assistant Four Started as {self.four.name}")
-
-        if config.STRING5:
-            await self.five.start()
-            try:
-                await self.one.join_chat("TheAltron")
-            except:
-                pass
-            assistants.append(5)
-            get_me = await self.five.get_me()
-            self.five.username = get_me.username
-            self.five.id = get_me.id
-            self.five.mention = get_me.mention
-            assistantids.append(get_me.id)
-            if get_me.last_name:
-                self.five.name = get_me.first_name + " " + get_me.last_name
-            else:
-                self.five.name = get_me.first_name
-            try:
-                await self.five.send_message(
-                    config.LOG_GROUP_ID, f"**» {config.MUSIC_BOT_NAME} ᴀssɪsᴛᴀɴᴛ ғɪᴠᴇ sᴛᴀʀᴛᴇᴅ :**\n\n❄ ɴᴀᴍᴇ : {self.five.name}\n✨ ɪᴅ : `{self.five.id}`\n💫 ᴜsᴇʀɴᴀᴍᴇ : @{self.five.username}"
-                )
-            except:
-                LOGGER(__name__).error(
-                    f"Assistant Account 5 has failed to access the log Group. Make sure that you have added your assistant to your log group and promoted as admin ! "
-                )
-                sys.exit()
-            LOGGER(__name__).info(f"Assistant Five Started as {self.five.name}")
