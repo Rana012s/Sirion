8a289c2c86e91503cd7c133281197fbc19873799
ExoticHero <126850195+ExoticHero@users.noreply.github.com>
2023-08-05 23:25:02 +0530

    Sirion

--- Sirion/plugins/speedtest.py
+++ Sirion/plugins/speedtest.py
@@ -1,15 +1,11 @@
 import asyncio
 import speedtest
-
 from pyrogram import filters
-
 from strings import get_command
-
 from Sirion import app
 from Sirion.misc import SUDOERS
 
 
-# Commands
 SPEEDTEST_COMMAND = get_command("SPEEDTEST_COMMAND")
 
 
