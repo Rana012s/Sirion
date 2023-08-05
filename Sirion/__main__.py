15c2567d4e27ab1859531750c15c8cd5adfa003c
ExoticHero <126850195+ExoticHero@users.noreply.github.com>
2023-08-05 12:50:46 +0530

    Sirion

--- Sirion/__main__.py
+++ Sirion/__main__.py
@@ -5,6 +5,7 @@ from os import mkdir, path
 from pyrogram import idle
 from pytgcalls.exceptions import NoActiveGroupCall
 from Sirion import LOGGER, app, userbot, HELPABLE
+from Sirion import *
 from Sirion.core.call import AltCall
 from Sirion.plugins import ALL_MODULES
 
@@ -14,7 +15,6 @@ loop = asyncio.get_event_loop()
 
 async def init():
     global BOT_ID, BOT_NAME, BOT_USERNAME, BOT_MENTION
-    global ID, NAME, USERNAME, MENTION
     
     if not path.exists("/files/"):
         mkdir("/files/")
@@ -41,26 +41,22 @@ async def init():
         imported_module = importlib.import_module("Sirion.plugins." + all_module)
         if (hasattr(imported_module, "__MODULE__") and imported_module.__MODULE__):
             imported_module.__MODULE__ = imported_module.__MODULE__
+            
             if (hasattr(imported_module, "__HELP__") and imported_module.__HELP__):
                 HELPABLE[imported_module.__MODULE__.lower()] = imported_module
+                
         print(f"Imported: {all_module}.py")
     LOGGER("Sirion.plugins").info("Necessary Modules Imported Successfully.")
     
     await userbot.start()
-    getmee = await userbot.get_me()
-    ID = getmee.id
-    MENTION = getmee.mention    
-    NAME = getmee.first_name + (getmee.last_name or "")
-    USERNAME = getmee.username
-
     await AltCall.start()
-    
     try:
         await AltCall.stream_call("https://te.legra.ph/file/29f784eb49d230ab62e9e.mp4")
     except NoActiveGroupCall:
         LOGGER("Sirion").error("[ERROR] - \n\nPlease turn on your Logger Group's Voice Call")
     except:
         pass
+        
     await AltCall.decorators()
     LOGGER("Sirion").info("Riruru Music Bot Started Successfully")
     await idle()
