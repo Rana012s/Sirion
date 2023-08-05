3b73424bd47d27d5828cb5faa20853aa578ebe6c
ExoticHero <126850195+ExoticHero@users.noreply.github.com>
2023-08-05 13:08:17 +0530

    Sirion

---
+++ Sirion/utils/inline/start.py
@@ -0,0 +1,36 @@
+from config import SUPPORT_HEHE
+from pyrogram.types import InlineKeyboardButton
+
+
+def start_pannel(BOT_USERNAME):
+    buttons = [
+        [
+            InlineKeyboardButton(
+                text="➕ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ ➕",
+                url=f"https://t.me/{BOT_USERNAME}?startgroup=new",
+            )
+        ],
+        [
+            InlineKeyboardButton(text="˹ʜᴇʟᴘ ᴍᴇɴᴜ˼", url=f"https://t.me/{BOT_USERNAME}?start=help"),
+            InlineKeyboardButton(text="⚙ sᴇᴛᴛɪɴɢs", callback_data="settings_helper"),
+        ],
+        ]
+    return buttons
+
+def private_panel(BOT_USERNAME):
+    buttons = [
+        [
+            InlineKeyboardButton(
+                text="➕ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ ➕",
+                url=f"https://t.me/{BOT_USERNAME}?startgroup=new",
+            )
+        ],
+        [
+            InlineKeyboardButton(text="˹ᴜᴘᴅᴀᴛᴇꜱ˼", url="https://t.me/TheAltron"),
+            InlineKeyboardButton(text="˹ꜱᴜᴘᴘᴏʀᴛ˼", url=f"https://t.me/{SUPPORT_HEHE}")
+        ],
+        [
+            InlineKeyboardButton(text="˹ʜᴇʟᴘ ᴍᴇɴᴜ˼", callback_data="settings_back_helper"),
+        ]
+    ]
+    return buttons
