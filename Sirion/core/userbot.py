169097415dfb0afab3f37d750bdb22cf37736d0f
ExoticHero <126850195+ExoticHero@users.noreply.github.com>
2023-08-06 17:33:05 +0530

    Pyrogram V2

--- Sirion/core/userbot.py
+++ Sirion/core/userbot.py
@@ -1,66 +1,67 @@
-from sys import exit
-from config import API_ID, API_HASH, STRING1, STRING2, LOG_GROUP_ID
+import sys
+import config
 from pyrogram import Client
 from Sirion.logging import LOGGER
 
 
+assistants = []
+assistantids = []
+
+
 class Userbot(Client):
     def __init__(self):
-        super().__init__(
-            api_id=API_ID,
-            api_hash=API_HASH,
-            no_updates=True,
+        self.one = Client(
+            name="Sirion1",
+            api_id=config.API_ID,
+            api_hash=config.API_HASH,
+            session_string=str(config.STRING1)
+        )
+        self.two = Client(
+            name="Sirion2",
+            api_id=config.API_ID,
+            api_hash=config.API_HASH,
+            session_string=str(config.STRING2)
         )
         
     async def start(self):
-        LOGGER(__name__).info("Getting Assistants Info")
-        
-        if STRING1:
-            one = Client(
-                api_id=API_ID,
-                api_hash=API_HASH,
-                name=str(STRING1),
-                no_updates=True,
-            )
-            await one.start()
+        LOGGER(__name__).info(f"Getting Assistants Info")
+        if config.STRING1:
+            await self.one.start()
             try:
-                await one.join_chat("TeamSirion")
-            except Exception as e:
-                LOGGER(__name__).error(f"Failed to join chat with Assistant 1: {e}")
+                await self.one.join_chat("TeamSirion")
+            except:
+                pass
+            assistants.append(1)
+            get_me = await self.one.get_me()
+            self.one.username = get_me.username
+            self.one.id = get_me.id
+            self.one.mention = get_me.mention
+            assistantids.append(get_me.id)
+            LOGGER(__name__).info(f"Assistant One Started")
             try:
-                get_me = await one.get_me()
-                one.username = get_me.username
-                one.id = get_me.id
-                one.mention = get_me.mention
-                LOGGER(__name__).info(f"Assistant 1 Started")
-                await one.send_message(LOG_GROUP_ID, f"**» Assistant 1 Started:** {one.mention}")
-            except Exception as e:
-                LOGGER(__name__).error(f"Failed to start Assistant 1: {e}")
-                exit()
-        
-        if STRING2:
-            two = Client(
-                api_id=API_ID,
-                api_hash=API_HASH,
-                name=str(STRING2),
-                no_updates=True,
-            )
-            await two.start()
-            try:
-                await two.join_chat("TeamSirion")
-            except Exception as e:
-                LOGGER(__name__).error(f"Failed to join chat with Assistant 2: {e}")
-            try:
-                get_me = await two.get_me()
-                two.username = get_me.username
-                two.id = get_me.id
-                two.mention = get_me.mention
-                LOGGER(__name__).info(f"Assistant 2 Started")
-                await two.send_message(LOG_GROUP_ID, f"**» Assistant 2 Started:** {two.mention}")
-            except Exception as e:
-                LOGGER(__name__).error(f"Failed to start Assistant 2: {e}")
-                exit()
+                await self.one.send_message(config.LOG_GROUP_ID, f"**» ᴀssɪsᴛᴀɴᴛ ᴏɴᴇ sᴛᴀʀᴛᴇᴅ :** {self.one.mention}")
+            except:
+                LOGGER(__name__).error(f"Assistant Account 1 has failed to access the log Group")
+                sys.exit()
 
+        if config.STRING2:
+            await self.two.start()
+            try:
+                await self.two.join_chat("TeamSirion")
+            except:
+                pass
+            assistants.append(2)
+            get_me = await self.two.get_me()
+            self.two.username = get_me.username
+            self.two.id = get_me.id
+            self.two.mention = get_me.mention
+            assistantids.append(get_me.id)
+            LOGGER(__name__).info(f"Assistant Two Started")
+            try:
+                await self.two.send_message(config.LOG_GROUP_ID, f"**» ᴀssɪsᴛᴀɴᴛ ᴛᴡᴏ sᴛᴀʀᴛᴇᴅ :** {self.two.mention}")
+            except:
+                LOGGER(__name__).error(f"Assistant Account 2 has failed to access the log Group")
+                sys.exit()
+            
 
 userbot = Userbot()
-
