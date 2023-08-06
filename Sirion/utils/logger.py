83fe45b3983242c405734775bfc78544f8b6fe6f
ExoticHero <126850195+ExoticHero@users.noreply.github.com>
2023-08-06 21:12:40 +0530

    Sirion

--- Sirion/utils/logger.py
+++ Sirion/utils/logger.py
@@ -5,20 +5,18 @@ from Sirion.utils.database import is_on_off
 
 async def play_logs(message, streamtype):
     if await is_on_off(LOG):
-
         if message.chat.id != LOG_GROUP_ID:
             if message.chat.username:
                 chatusername = f"@{message.chat.username}"
             else:
                 chatusername = "ᴩʀɪᴠᴀᴛᴇ ᴄʜᴀᴛ"
-            logger_text = f"""**{MUSIC_BOT_NAME} ᴩʟᴀʏ ʟᴏɢɢᴇʀ**
+            logger_text = f"""**{BOT_MENTION} ᴩʟᴀʏ ʟᴏɢɢᴇʀ**
 
 **ᴄʜᴀᴛ:** {message.chat.title} [`{message.chat.id}`]
 **ᴜsᴇʀ:** {message.from_user.mention}
 **ᴜsᴇʀɴᴀᴍᴇ:** @{message.from_user.username}
 **ɪᴅ:** `{message.from_user.id}`
 **ᴄʜᴀᴛ ʟɪɴᴋ:** {chatusername}
-
 **sᴇᴀʀᴄʜᴇᴅ ғᴏʀ:** {message.text}
 
 **sᴛʀᴇᴀᴍ ᴛʏᴩᴇ:** {streamtype}"""
